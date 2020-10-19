bee2ad3c7295f047a4f4aab17c1e7192f4d13e81
shibatea <ggl.takashi.68@gmail.com>
2020-10-19 15:47:23 +0900

    impl: Circle オブジェクトを追加

---
+++ DddStudy/DddStudy.Domain/Models/Circles/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using DddStudy.Domain.Core.Models;
+using DddStudy.Domain.Models.Users;
+
+namespace DddStudy.Domain.Models.Circles
+{
+    public class Circle : ValueObject<Circle>
+    {
+        public Circle(CircleId id, CircleName name, User owner, List<User> members)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (owner == null) throw new ArgumentNullException(nameof(owner));
+            if (members == null) throw new ArgumentNullException(nameof(members));
+
+            Id = id;
+            Name = name;
+            Owner = owner;
+            Members = members;
+        }
+
+        public CircleId Id { get; }
+        public CircleName Name { get; }
+        public User Owner { get; }
+        public List<User> Members { get; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Id;
+        }
+    }
+}
