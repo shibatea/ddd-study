a7fd12b9d4030cea97a331229982b5dd46f724c5
shibatea <ggl.takashi.68@gmail.com>
2020-10-19 15:27:12 +0900

    impl: ライフサイクルのあるサークルオブジェクト（＝エンティティ）の識別子となる値オブジェクト CircleId を実装

---
+++ DddStudy/DddStudy.Domain/Models/Circles/CircleId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using DddStudy.Domain.Core.Models;
+
+namespace DddStudy.Domain.Models.Circles
+{
+    public class CircleId : ValueObject<CircleId>
+    {
+        public CircleId(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            if (value.Length < 3) throw new ArgumentException("サークル名は3文字以上です。", nameof(value));
+            if (value.Length < 20) throw new ArgumentException("サークル名は20文字以下です。", nameof(value));
+
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
