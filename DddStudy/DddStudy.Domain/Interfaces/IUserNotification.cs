1385c0431bc8ec1d8e8fb9a0fef33b9db707a5c2
shibatea <ggl.takashi.68@gmail.com>
2020-10-21 05:40:31 +0900

    impl: ドメインの知識を infra に漏らさないための施策として通知用インタフェースを実装したときのコミット漏れ

---
+++ DddStudy/DddStudy.Domain/Interfaces/IUserNotification.cs
@@ -0,0 +1,10 @@
+﻿using DddStudy.Domain.Models.Users;
+
+namespace DddStudy.Domain.Interfaces
+{
+    public interface IUserNotification
+    {
+        void Id(UserId id);
+        void Name(UserName name);
+    }
+}
