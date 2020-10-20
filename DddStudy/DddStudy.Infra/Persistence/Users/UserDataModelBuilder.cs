2defffc7ba4adf812b84283fd240249e3c82bbda
shibatea <ggl.takashi.68@gmail.com>
2020-10-20 09:54:57 +0900

    impl: IUserNotification インタフェースを実装しており、User クラスから隠蔽されたプロパティを受け取ることができる。受け取ったプロパティを使って、データモデルの生成までを行うクラス。

---
+++ DddStudy/DddStudy.Infra/Persistence/Users/UserDataModelBuilder.cs
@@ -0,0 +1,32 @@
+﻿using DddStudy.Domain.Models.Users;
+using DddStudy.Infra.Persistence.DataModels;
+
+namespace DddStudy.Infra.Persistence.Users
+{
+    public class UserDataModelBuilder : IUserNotification
+    {
+        // 通知されたデータはインスタンス変数で保持される
+        private UserId _id;
+        private UserName _name;
+
+        public void Id(UserId id)
+        {
+            _id = id;
+        }
+
+        public void Name(UserName name)
+        {
+            _name = name;
+        }
+
+        // 通知されたデータからデータモデルを生成するメソッド
+        public UserDataModel Build()
+        {
+            return new UserDataModel
+            {
+                Id = _id.Value,
+                Name = _name.Value
+            };
+        }
+    }
+}
